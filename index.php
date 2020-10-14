88439de7f214cb6695314393df65cfcd8730bd42
raphael anderson <raphael.8379@hotmail.com>
2020-10-14 15:32:59 -0300

    criado arquivo index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="css/style.css" />
+<title>Raphael Anderson RA</title>
+</head>
+
+<body>
+				   <!--  Header-->
+             <header>   
+				  <nav role='navigation'>
+       	 <a href="../../index.php">Home</a>
+          <a href="#">Sobre</a>
+          <a href="../../contato.php">Contato</a>
+        </nav>
+
+             
+             </header>
+         <br />    
+           
+             			
+                        <div class="box-tabs">
+                            <div style="background-color: #2A2D34; font-weight: bold" class="single-tab" align="center">Missão</div>
+                            <div class="single-tab" align="center">Visão</div>
+                            <div class="single-tab" align="center">Valores</div>
+                        </div>
+    
+                        <div id="box-text" align="center">
+     Clique na Missão, Visão e Valores para visualizar o efeito <br /> Unselect - Select criado com javascript puro
+                        </div>
+                        
+                        
+                        
+                          <script src="js/scripts.js"></script>
+</body>
+</html>
