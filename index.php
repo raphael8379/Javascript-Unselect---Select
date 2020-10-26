b79e4269c09c27b8f9f4a3c8cc6344e40b166189
raphael anderson <raphael.8379@hotmail.com>
2020-10-26 06:37:18 -0300

    arquivo index.php alterado

--- index.php
+++ index.php
@@ -3,6 +3,13 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" href="css/style.css" />
+<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+<link rel="manifest" href="favicon/site.webmanifest">
+<link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
 <title>Raphael Anderson RA</title>
 </head>
 
@@ -11,7 +18,7 @@
              <header>   
 				  <nav role='navigation'>
        	 <a href="../../index.php">Home</a>
-          <a href="#">Sobre</a>
+         
           <a href="../../contato.php">Contato</a>
         </nav>
 
@@ -27,7 +34,7 @@
                         </div>
     
                         <div id="box-text" align="center">
-     Clique na Missão, Visão e Valores para visualizar o efeito <br /> Unselect - Select criado com javascript puro
+    <h3> Clique na Missão, Visão e Valores para visualizar o efeito  Unselect - Select criado com javascript puro  </h3>
                         </div>
                         
                         
